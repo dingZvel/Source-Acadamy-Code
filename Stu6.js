787827ed87fbbb7ba4a63725affbfee4f3c249dc
Source Academy User <dingZvel>
2023-09-19 12:40:49 +0800

    Changes made from Source Academy

--- Stu6.js
+++ Stu6.js
@@ -12,7 +12,7 @@ function remove_duplicates(lst) {
              remove_duplicates(filter(x => x !== head(lst), tail(lst))));
 }
 //remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2));
-remove_duplicates(list("a", "x", "b", "c", "c", "b", "d"));
+//remove_duplicates(list("a", "x", "b", "c", "c", "b", "d"));
 
 //Q3
 function makeup_amount(x, coins) {
@@ -22,16 +22,16 @@ function makeup_amount(x, coins) {
         return null;
     } else {
         // Combinations that do not use the head coin.
-        const combi_A = ...
+        const combi_A = makeup_amount(x, tail(coins));
         
         // Combinations that do not use the head coin
         // for the remaining amount.
-        const combi_B = ...
+        const combi_B = makeup_amount(x - head(coins), tail(coins));
         
         // Combinations that use the head coin.
-        const combi_C = ...
+        const combi_C = map(x => pair(head(coins), x), combi_B);
         
         return append(combi_A, combi_C);
     }
 }
-makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50));
+display_list(makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50)));
